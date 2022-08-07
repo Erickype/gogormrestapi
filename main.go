055efa5e35b6b82f30a118e35220d9cdfef91e8d
Erickype <erickype@hotmail.com>
2022-08-06 20:29:55 -0500

    Server initial config listening in port 3000

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hola mundo"))
+	})
+
+	http.ListenAndServe(":3000", router)
 }
